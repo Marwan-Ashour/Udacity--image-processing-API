aedac8779f8e9757e406676a16edfd71f296d041
Marwan-Ashour <92494162+Marwan-Ashour@users.noreply.github.com>
2021-12-26 22:36:59 +0200

    Update checkImage.ts

--- src/coordinator/checkImage.ts
+++ src/coordinator/checkImage.ts
@@ -1,20 +1,31 @@
-// import file system promises
+// import fileSystem promisess
 import fs  from "fs";
+import { promises as fspromises } from "fs";
 import path from "path";
 import sharp from "sharp"
 // checking image availability
-const checkImage =  (
+const checkImage = async (
   imageName: string,
   width: number,
   height: number
-) => {
+  ) : Promise<string> => {
   const resizedDir = path.join(__dirname, "../../images/thumbnails/");
   
   const file_path = path.join(resizedDir,`${imageName.toLowerCase()}(${width}x${height}).jpg`);
 
  
-  return fs.existsSync(file_path);
+  // return fs.existsSync(file_path);
+
+  
+  try {
+    await fspromises.readFile(file_path, { flag: "r" });
+    return file_path;
+  } catch {
+    return "";
+  }
+
    
 };
 
 export default checkImage;
+
